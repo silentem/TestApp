f9bf1cb344817a33b5a1e505f3552cc718db00ed
whaletail <silentem1113@gmail.com>
2018-03-20 21:29:00 +0200

    add base activities

---
+++ app/src/main/java/com/whaletail/uklon/test/mvp/postDetails/PostDetailsActivity.kt
@@ -0,0 +1,13 @@
+package com.whaletail.uklon.test.mvp.postDetails
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.whaletail.uklon.test.R
+
+class PostDetailsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_post_details)
+    }
+}
--- app/src/main/java/com/whaletail/uklon/test/mvp/posts/PostsActivity.kt
+++ app/src/main/java/com/whaletail/uklon/test/mvp/posts/PostsActivity.kt
@@ -1,12 +1,13 @@
-package com.whaletail.uklon.test
+package com.whaletail.uklon.test.mvp.posts
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.whaletail.uklon.test.R
 
-class MainActivity : AppCompatActivity() {
+class PostsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_posts)
     }
 }
